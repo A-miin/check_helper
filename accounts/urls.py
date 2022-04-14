4776dddb9b4ea4d720286676ac0aaff0eb8f45e7
A-miin <zulaykaisaeva@gmail.com>
2022-04-15 01:58:41 +0600

    Add permissions for list, retrieve, update and delete user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,18 +1,17 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from accounts.views import UserViewSet
-from rest_framework.routers import DefaultRouter
+from accounts.views import (
+    ListCreateUsersAPIView,
+    RetrieveUpdateDestroyUsersAPIView,
+)
 
-app_name = "accounts"
-
-user_router = DefaultRouter()
-user_router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include(user_router.urls)),
+    path('users/', ListCreateUsersAPIView.as_view(), name='users-list-create'),
+    path('users/<int:pk>/', RetrieveUpdateDestroyUsersAPIView.as_view(), name='users-rud')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,33 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext_lazy as _
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
 from accounts.serializers import UserSerializer
-from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.response import Response
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = get_user_model().objects.all()
+class ListCreateUsersAPIView(ListCreateAPIView):
     serializer_class = UserSerializer
-    # permission_classes = [IsAccountAdminOrReadOnly]
+    queryset = get_user_model().objects.all()
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return Response({'detail': _('You do not have permission to perform this action.')},
+                            status=status.HTTP_403_FORBIDDEN)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class RetrieveUpdateDestroyUsersAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
+
+    def get_object(self):
+        user = super(RetrieveUpdateDestroyUsersAPIView, self).get_object()
+        if self.request.user.is_staff:
+            return user
+        else:
+            return self.request.user
